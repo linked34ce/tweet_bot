9426c3cb4c0577c83857eb795d705d63ebe4542f
linked34ce <9797ouki@gmail.com>
2023-07-26 23:09:07 +0900

    find quote by character id

--- src/quote_dao.py
+++ src/quote_dao.py
@@ -1,3 +1,4 @@
+from random import randint
 from sqlite3 import Connection, Cursor, Error
 from connection_manager import ConnectionManager
 from quote import Quote
@@ -8,20 +9,22 @@ class QuoteDAO:
     def __init__(self, conn: Connection) -> None:
         self.conn = conn
 
-    def findQuoteById(self, id: int) -> Quote:
-        sql: str = "select * from (Quotes inner join Episodes on Quotes.episode = Episodes.id) " \
-                   "inner join Characters on Quotes.character = Characters.id where Quotes.id = ?;"
+    def find_quote_by_quote_id(self, quote_id: int) -> Quote:
+        sql: str = "SELECT * FROM (Quotes INNER JOIN Episodes ON Quotes.episode = Episodes.id) " \
+                   "INNER JOIN Characters ON Quotes.character = Characters.id WHERE Quotes.id = ?;"
         cur: Cursor = None
-        params: tuple = (id, )
+        params: tuple = (quote_id, )
         res: tuple = ()
         quote: Quote = None
 
         try:
             self.conn = ConnectionManager.getConnection()
             cur = self.conn.cursor()
+
             cur.execute(sql, params)
             res = cur.fetchone()
-            quote = Quote(res["episode"], res["title"], res["character"], res["quote"])
+            
+            quote = Quote(res["episode"], res["title"], res["name"], res["quote"])
 
         except Error:
             print("database error occurred")
@@ -32,16 +35,44 @@ class QuoteDAO:
 
         return quote
 
-    def findQuoteByCharacter(character: str):
+    def find_quote_by_character_id(self, character_id: int) -> Quote:
+        sql: str = "SELECT * FROM (Quotes INNER JOIN Episodes ON Quotes.episode = Episodes.id) " \
+                   "INNER JOIN Characters ON Quotes.character = Characters.id WHERE Characters.id = ?;"
+        cur: Cursor = None
+        params: tuple = (character_id, )
+        sum_of_quotes: int = 0
+        random_index: int = 0
+        res: tuple = ()
+        quote: Quote = None
+
+        try:
+            self.conn = ConnectionManager.getConnection()
+            cur = self.conn.cursor()
+
+            sum_of_quotes = self.get_sum_of_quotes_by_character_id(character_id)
+            random_index = randint(0, sum_of_quotes - 1)
+
+            cur.execute(sql, params)
+            res = cur.fetchall()[random_index]
+            
+            quote = Quote(res["episode"], res["title"], res["name"], res["quote"])
+
+        except Error:
+            print("database error occurred")
+
+        finally:
+            if cur != None:
+                cur.close()
+
+        return quote
+
+    def find_quote_by_episode(self, episode: int):
         pass
 
-    def findQuoteByEpisode(episode: int):
+    def find_quote_by_character_id_and_episode(self, character_id: int, episode: int):
         pass
 
-    def findQuoteByCharacterAndEpisode(episode: int):
-        pass
-
-    def getSumOfQuotes(self) -> int:
+    def get_sum_of_all_quotes(self) -> int:
         sql: str = "SELECT COUNT(*) AS count FROM Quotes;"
         cur: Cursor = None
         res: tuple = ()
@@ -50,8 +81,10 @@ class QuoteDAO:
         try:
             self.conn = ConnectionManager.getConnection()
             cur = self.conn.cursor()
+
             cur.execute(sql)
             res = cur.fetchone()
+
             sum_of_quotes = res["count"]
 
         except Error:
@@ -62,6 +95,37 @@ class QuoteDAO:
                 cur.close()
 
         return sum_of_quotes
+    
+    def get_sum_of_quotes_by_character_id(self, character_id: int) -> int:
+        sql: str = "SELECT COUNT(*) AS count FROM Quotes WHERE character = ?;"
+        cur: Cursor = None
+        params: tuple = (character_id, )
+        res: tuple = ()
+        sum_of_quotes: int = 0
+
+        try:
+            self.conn = ConnectionManager.getConnection()
+            cur = self.conn.cursor()
+
+            cur.execute(sql, params)
+            res = cur.fetchone()
+
+            sum_of_quotes = res["count"]
+
+        except Error:
+            print("database error occurred")
+
+        finally:
+            if cur != None:
+                cur.close()
+
+        return sum_of_quotes
+    
+    def get_sum_of_quotes_by_episode(self, episode: int) -> int:
+        pass
+
+    def get_sum_of_quotes_by_character_id_and_episode(self, character_id: int, episode: int) -> int:
+        pass
     
     def getConn(self) -> Connection:
         return self.conn
--- src/test.py
+++ src/test.py
@@ -6,16 +6,36 @@ from quote import Quote
 try: 
     conn: Connection = ConnectionManager.getConnection()
     quote_dao = QuoteDAO(conn)
+    quote: Quote = None
+    status: str = ""
+    sum_of_quotes: int = 0
+    character_id: int = 0
+    quote_id: int = 0
 
-    id: int = 1
-    quote: Quote = quote_dao.findQuoteById(1)
-    status: str = "{}\n───────────────\n{}\n第{}話「{}」".format(
-                    quote.quote, quote.character, quote.episode, quote.title
-                )
+    sum_of_quotes = quote_dao.get_sum_of_all_quotes()
+    print("get_sum_of_all_quotes() -> " + str(sum_of_quotes))
+    print()
+
+    character_id = 1  # Yu
+    sum_of_quotes = quote_dao.get_sum_of_quotes_by_character_id(character_id)
+    print("get_sum_of_quotes_by_character_id() -> " + str(sum_of_quotes))
+    print()
+
+    quote_id = 1
+    quote = quote_dao.find_quote_by_quote_id(quote_id)
+    status = "{}\n───────────────\n{}\n第{}話「{}」".format(
+                quote.quote, quote.character, quote.episode, quote.title
+            )
     print(status)
+    print()
 
-    sum_of_quotes: int = quote_dao.getSumOfQuotes()
-    print(sum_of_quotes)
+    character_id = 2  # Ayumu
+    quote = quote_dao.find_quote_by_character_id(character_id)
+
+    status = "{}\n───────────────\n{}\n第{}話「{}」".format(
+                quote.quote, quote.character, quote.episode, quote.title
+            )
+    print(status)
 
 except Error:
     print("database error occurred")
